c706cfbe487a10c0156a18289198a6abd270f1d5
Evan Johnson <ecrjmegamanx@gmail.com>
2026-06-04 03:45:44 -0400

    switched model file to snake case

--- src/model.py
+++ src/model.py
@@ -5,13 +5,13 @@ import torch.nn.functional as F
 # Deep Q-Network (DQN) model class
 # This class defines the architecture of the DQN used by the agent
 class DQN(nn.Module):
-    def __init__(self, dState, dAction):
+    def __init__(self, d_state, d_action):
         # Follow the parent class constructor
         super().__init__()
         # Initialize three linear layers
-        self.layer1 = nn.Linear(dState, 256)
+        self.layer1 = nn.Linear(d_state, 256)
         self.layer2 = nn.Linear(256, 128)
-        self.layer3 = nn.Linear(128, dAction)
+        self.layer3 = nn.Linear(128, d_action)
 
     # Define the forward pass
     def forward(self, x):
